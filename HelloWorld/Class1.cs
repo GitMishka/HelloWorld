c740763762324aa8a844818a608a3485f8ef6f6b
Misha <mishagrigoryan@gmail.com>
2023-08-19 00:35:55 -0400

    wdwad

--- HelloWorld/Class1.cs
+++ HelloWorld/Class1.cs
@@ -1,30 +1,19 @@
 ﻿using System;
 
-namespace HelloWorld
+Console.Write("Enter the first number: ");
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter the second number: ");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"Sum: {num1 + num2}");
+Console.WriteLine($"Difference: {num1 - num2}");
+Console.WriteLine($"Product: {num1 * num2}");
+if (num2 != 0) // To avoid division by zero
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-
-            Console.Write("Enter the first number: ");
-            double num1 = Convert.ToDouble(Console.ReadLine());
-
-            Console.Write("Enter the second number: ");
-            double num2 = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine($"Sum: {num1 + num2}");
-            Console.WriteLine($"Difference: {num1 - num2}");
-            Console.WriteLine($"Product: {num1 * num2}");
-            if (num2 != 0) // To avoid division by zero
-            {
-                Console.WriteLine($"Quotient: {num1 / num2}");
-            }
-            else
-            {
-                Console.WriteLine("Cannot divide by zero.");
-            }
-
-        }
-    }
+    Console.WriteLine($"Quotient: {num1 / num2}");
+}
+else
+{
+    Console.WriteLine("Cannot divide by zero.");
 }
