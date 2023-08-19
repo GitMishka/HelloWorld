86b3003dbcae4f242de9de5b42097d58270c2bab
Misha <mishagrigoryan@gmail.com>
2023-08-19 00:14:54 -0400

    guesser

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,8 +1,26 @@
-<<<<<<< HEAD
-﻿using System;
-Console.Write("Enter your name: ");
-string name = Console.ReadLine();
-Console.WriteLine($"Hello, {name}!");
-=======
-﻿// See https://aka.ms/new-console-template for more information
->>>>>>> a4bcde17f51c12524fd324e46fa48e309c0f574c
+using System;
+
+Random rand = new();
+int secretNumber = rand.Next(1, 101); // Random number between 1 and 100
+
+Console.WriteLine("Guess the number between 1 and 100!");
+
+int guess;
+do
+{
+    Console.Write("Enter your guess: ");
+    guess = Convert.ToInt32(Console.ReadLine());
+
+    if (guess > secretNumber)
+    {
+        Console.WriteLine("Too high!");
+    }
+    else if (guess < secretNumber)
+    {
+        Console.WriteLine("Too low!");
+    }
+    else
+    {
+        Console.WriteLine("Congratulations! You guessed the correct number!");
+    }
+} while (guess != secretNumber);
