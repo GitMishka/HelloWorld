2a780d70695b4fce9fdb33c31393125a35e9eaa0
Misha <mishagrigoryan@gmail.com>
2023-08-19 00:00:54 -0400

    3

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -5,5 +5,4 @@ string name = Console.ReadLine();
 Console.WriteLine($"Hello, {name}!");
 =======
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 >>>>>>> a4bcde17f51c12524fd324e46fa48e309c0f574c
