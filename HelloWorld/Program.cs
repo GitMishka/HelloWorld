f32a0053f80eab1326cfafe4f25835e1ad0fcbb0
Misha <mishagrigoryan@gmail.com>
2023-08-20 22:35:30 -0400

    output in one column

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using Npgsql;
 
@@ -10,6 +11,17 @@ namespace PostgreSQLExample
         {
             var connectionString = "Host=mishasdb.postgres.database.azure.com;Username=misha;Password=Manonthemoon123;Database=azure_app";
 
+
+            // Assuming these as initial maximum lengths based on headers
+            int maxDateLength = "Date".Length;
+            int maxUsernameLength = "Username".Length;
+            int maxTweetLength = "Tweet".Length;
+
+            // Create lists to temporarily hold data since we need two passes
+            var dates = new List<DateTime>();
+            var usernames = new List<string>();
+            var tweets = new List<string>();
+
             try
             {
                 using var connection = new NpgsqlConnection(connectionString);
@@ -18,25 +30,33 @@ namespace PostgreSQLExample
                 using var dataCmd = new NpgsqlCommand("SELECT date, username, tweet FROM tweets", connection);
                 using var reader = dataCmd.ExecuteReader();
 
-                // Path to the file where data will be saved
-                string filePath = @"C:\Users\Misha\source\repos\HelloWorld\output.csv";
-
-                // Using StreamWriter to write data to the file
-                using StreamWriter fileWriter = new StreamWriter(filePath);
-
-                // Write the header line
-                fileWriter.WriteLine("Date,Username,Tweet");
-
                 while (reader.Read())
                 {
                     DateTime dateValue = reader.GetDateTime(0);
                     string usernameValue = reader.GetString(1);
-                    string tweetValue = reader.GetString(2).Replace(",", ";"); // To ensure no commas within tweets mess up the format
+                    string tweetValue = reader.GetString(2);
 
-                    string outputLine = $"{dateValue},{usernameValue},{tweetValue}";
-                    fileWriter.WriteLine(outputLine);
+                    // Check and update max lengths
+                    maxDateLength = Math.Max(maxDateLength, dateValue.ToString("M/d/yyyy H:mm").Length);
+                    maxUsernameLength = Math.Max(maxUsernameLength, usernameValue.Length);
+                    maxTweetLength = Math.Max(maxTweetLength, tweetValue.Length);
+
+                    // Store data temporarily
+                    dates.Add(dateValue);
+                    usernames.Add(usernameValue);
+                    tweets.Add(tweetValue);
                 }
 
+                string filePath = @"C:\Users\Misha\source\repos\HelloWorld\output.csv";
+
+
+                using StreamWriter fileWriter = new StreamWriter(filePath);
+                fileWriter.WriteLine($"{"Date".PadRight(maxDateLength)}\t{"Username".PadRight(maxUsernameLength)}\t{"Tweet"}");
+
+                for (int i = 0; i < dates.Count; i++)
+                {
+                    fileWriter.WriteLine($"{dates[i].ToString("M/d/yyyy H:mm").PadRight(maxDateLength)}\t{usernames[i].PadRight(maxUsernameLength)}\t{tweets[i]}");
+                }
 
                 Console.WriteLine("Data has been written to the file successfully!");
 
@@ -52,5 +72,3 @@ namespace PostgreSQLExample
         }
     }
 }
-
-//"Host=mishasdb.postgres.database.azure.com;Username=misha;Password=;Database=azure_app";
