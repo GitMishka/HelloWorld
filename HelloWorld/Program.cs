a1e75a4c673fbb2e8e1384adbc78714ca427a291
Misha <mishagrigoryan@gmail.com>
2023-08-20 22:08:06 -0400

    db1

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,18 +1,49 @@
 using System;
 using Npgsql;
 
-class Program
+namespace PostgreSQLExample
 {
-    static void Main()
+    class Program
     {
-        var cs = "Host=mishasdb.postgres.database.azure.com;Username=misha;Password=;Database=azure_app";
+        static void Main()
+        {
+            var connectionString = "Host=mishasdb.postgres.database.azure.com;Username=misha;Password=;Database=azure_app";
 
-        using var con = new NpgsqlConnection(cs);
-        con.Open();
+            try
+            {
+                // Establishing a connection to the PostgreSQL database
+                using var connection = new NpgsqlConnection(connectionString);
+                connection.Open();
 
-        using var cmd = new NpgsqlCommand("SELECT version()", con);
-        var version = cmd.ExecuteScalar().ToString();
-        Console.WriteLine($"PostgreSQL version: {version}");
+                // Fetching PostgreSQL version
+                using (var versionCmd = new NpgsqlCommand("SELECT version()", connection))
+                {
+                    var version = versionCmd.ExecuteScalar().ToString();
+                    Console.WriteLine($"PostgreSQL version: {version}");
+                }
+
+                // Retrieving data from a table
+                using (var dataCmd = new NpgsqlCommand("SELECT id, name FROM mytable", connection))
+                using (var reader = dataCmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(reader.GetInt32(0) + ": " + reader.GetString(1));
+                    }
+                }
+            }
+            catch (NpgsqlException ex)
+            {
+                // Handle exceptions related to Npgsql
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                // Handle any general exceptions
+                Console.WriteLine($"An unexpected error occurred: {ex.Message}");
+            }
+        }
     }
 }
+
 //"Host=mishasdb.postgres.database.azure.com;Username=misha;Password=;Database=azure_app";
